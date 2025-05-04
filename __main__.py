c73c6191fd5b467a3bc40e3273ff4f3d28091cd6
AlexM-2 <198247789+AlexM-2@users.noreply.github.com>
2025-05-04 16:48:11 +0100

    Added mouse click event functionality, Mouse class, Mouse_state class, using pygame.time.tick rather than time.sleep, removed Group, Rect, Circle, and Coordinate classes in favour of pygame.sprite.Sprite and pygame.sprite.Group. Only certain events defined in allowed_events are allowed to enter the event queue. Added pygame.quit() when the player quits and changed exit() to raise SystemExit.

--- __main__.py
+++ __main__.py
@@ -1,60 +1,102 @@
-import time
-
 import pygame
 
 pygame.init()
 
-class Coordinate:
-    def __init__(self, x: float, y: float):
-
-        x = float(x)
-        y = float(y)
-
-        super(tuple).__init__(tuple, (x, y))
-
-        self.x = x
-        self.y = y
-        print("correct")
-    
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-class Circle:
-    def __init__(self, parent: pygame.Surface, center: Coordinate, radius: float, color: pygame.Color):
-        self.parent = parent
-        self.center = center
-        self.radius = radius
-        self.color = color
-    
-    def draw(self):
-        pygame.draw.circle(self.parent, self.color, self.center, self.radius)
-
-class Rect:
-    def __init__(self, position: Coordinate, size: Coordinate, color: pygame.Color, border: float, full_border: tuple[float, float, float, float]):
-        self.postion = position
-
-        self.size = size
-
-class Group:
-    def __init__(self, *objects: pygame.Rect | pygame.Surface | Circle | Rect):
-        pass
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-playButton = pygame.Rect(300, 600, 200, 100)
+clock = pygame.time.Clock()
+
+allowed_events = [
+    pygame.QUIT,
+    pygame.MOUSEBUTTONDOWN,
+    pygame.MOUSEBUTTONUP,
+]
+pygame.event.set_blocked(None) #None means all will be blocked
+for event in allowed_events:
+    pygame.event.set_allowed(event)
+
+class Mouse_state(list):
+
+    def __new__(cls, *args):
+        return super().__new__(cls, args)
+
+    def __init__(self, state):
+        
+        self.state = state
+
+        self.mb1 = state[0]
+        self.append(state[0])
+
+        self.mb2 = state[1]
+        self.append(state[1])
+
+        self.mb3 = state[2]
+        self.append(state[2])
+
+        self.scroll_up = state[3]
+        self.append(state[3])
+
+        self.scroll_down = state[4]
+        self.append(state[4])
+        
+
+pygame.Vector2
+
+class Mouse:
+    def __init__(self, state: Mouse_state = (False, False, False, False, False),  pos: pygame.Vector2 = pygame.mouse.get_pos()):
+        self.pos: pygame.Vector2 = pygame.Vector2(pos[0], pos[1])
+
+        self.state: Mouse_state = Mouse_state(state)
+
+mouse = Mouse()
+
+playButton = pygame.sprite.Sprite()
 
 while True:
-    
-    for event in pygame.event.get():
+
+    all_events = pygame.event.get()
+    for event in all_events:
         if event.type == pygame.QUIT:
-            exit()
-            
-        screen.fill((3,6,22))
+            pygame.quit()
+            raise SystemExit
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse.pos = event.pos
+            if event.button == 1:
+                mouse.state.mb1 = True
+                mouse.state[0] = True
+            elif event.button == 2:
+                mouse.state.mb2 = True
+                mouse.state[1] = True
+            elif event.button == 3:
+                mouse.state.mb3 = True
+                mouse.state[2] = True
+            elif event.button == 4:
+                mouse.state.scroll_up = True
+                mouse.state[3] = True
+            elif event.button == 5:
+                mouse.state.scroll_down = True
+                mouse.state[4] = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse.pos = event.pos
+            if event.button == 1:
+                mouse.state.mb1 = False
+                mouse.state[0] = False
+            elif event.button == 2:
+                mouse.state.mb3 = False
+                mouse.state[1] = False
+            elif event.button == 3:
+                mouse.state.mb2 = False
+                mouse.state[2] = False
+            elif event.button == 4:
+                mouse.state.scroll_up = False
+                mouse.state[3] = False
+            elif event.button == 5:
+                mouse.state.scroll_down = False
+                mouse.state[4] = False
+        print(f"{mouse.state=}")
+    
+    screen.fill((3, 6, 22))
 
-        pygame.draw.rect(screen, (75, 75, 75), playButton)
-        # pygame.draw.circle()
-
-        pygame.display.update()
-        time.sleep(0.01)
+    pygame.display.update()
+    clock.tick(60)
