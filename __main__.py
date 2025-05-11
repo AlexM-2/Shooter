c2542e102b65ecb791e1b9c19467c6aa7682ad00
AlexM-2 <198247789+AlexM-2@users.noreply.github.com>
2025-05-11 10:06:02 +0100

    Changed the default state of a Mouse object to the current mouse state. Created classes: Rect, which can be drawn the screen with the values defined at initialisation or you can override them with optional parameters in the draw() method; Border (used in the Rect class); Border_val (used in the Border class). Moved the screen.fill(color) line to before the event loop. Using the newly created Rect class for the play button and using it's draw method to draw it onto the screen

--- __main__.py
+++ __main__.py
@@ -39,21 +39,227 @@ class Mouse_state(list):
 
         self.scroll_down = state[4]
         self.append(state[4])
-        
-
-pygame.Vector2
 
 class Mouse:
-    def __init__(self, state: Mouse_state = (False, False, False, False, False),  pos: pygame.Vector2 = pygame.mouse.get_pos()):
+    def __init__(self, state: Mouse_state = pygame.mouse.get_pressed(5),  pos: pygame.Vector2 = pygame.mouse.get_pos()):
         self.pos: pygame.Vector2 = pygame.Vector2(pos[0], pos[1])
 
         self.state: Mouse_state = Mouse_state(state)
 
+class Border_val(int):
+
+    def __new__(cls, val: int, shortened: bool, name: str):
+        return super(Border_val, cls).__new__(cls, val)
+
+    def __init__(self, val: int, shortened: bool, name: str):
+
+        super().__init__()
+
+        self.shortened: bool = shortened
+        self.name: str = name
+
+class Border(list):
+    def __init__(
+        self,
+        shortened: int = -1,
+        top_left: int = -1,
+        top_right: int = -1,
+        bottom_left: int = -1,
+        bottom_right: int = -1
+    ):
+        super().__init__()
+        
+        if top_left == -1:
+            self.top_left: Border_val = Border_val(shortened, True, "top_left")
+        else:
+            self.top_left: Border_val = Border_val(top_left, False, "top_left")
+        
+        if top_right == -1:
+            self.top_right: Border_val = Border_val(shortened, True, "top_right")
+        else:
+            self.top_right: Border_val = Border_val(top_right, False, "top_right")
+
+        if bottom_left == -1:
+            self.bottom_left: Border_val = Border_val(shortened, True, "bottom_left")
+        else:
+            self.bottom_left: Border_val = Border_val(bottom_left, False, "bottom_left")
+
+        if bottom_right == -1:
+            self.bottom_right: Border_val = Border_val(shortened, True, "bottom_right")
+        else:
+            self.bottom_right: Border_val = Border_val(bottom_right, False, "bottom_right")
+        
+        self.append(self.top_left)
+        self.append(self.top_right)
+        self.append(self.bottom_left)
+        self.append(self.bottom_right)
+        
+        if self.top_left.shortened == True and self.top_right.shortened == True and self.bottom_left.shortened == True and self.bottom_right.shortened == True:
+            self.is_shortened: bool = True
+            self.shortened: int = shortened
+
+            if shortened == -1:
+                self.is_default = True
+            else:
+                self.is_default = False
+        else:
+            self.is_shortened: bool = False
+            self.is_default = False
+    
+    # def __setattr__(self, name, value):
+    #     if name == "shortened":
+    #         self.__init__()
+    #     elif name == "top_left":
+    #         self[0] = value
+    #     elif name == "top_right":
+    #         self[1] = value
+    #     elif name == "bottom_left":
+    #         self[2] = value
+    #     elif name == "bottom_right":
+    #         self[3] = value
+    #     super().__setattr__(name, value)
+
+    # def __delattr__(self, name):
+    #     if name == "border":
+    #         self.__init__(-1, self.top_left, self.top_right, self.bottom_left, self.bottom_right)
+    #     elif name == "top_left":
+    #         self.__init__(self.shortened, -1, self.top_right, self.bottom_left, self.bottom_right)
+    #     elif name == "top_right":
+    #         self.__init__(self.shortened, self.top_left, -1, self.bottom_left, self.bottom_right)
+    #     elif name == "bottom_left":
+    #         self.__init__(self.shortened, self.top_left, self.top_right, -1, self.bottom_right)
+    #     elif name == "bottom_right":
+    #         self.__init__(self.shortened, -1, self.top_right, self.bottom_left, -1)
+    #     elif name == "border":
+    #         self.__init__(-1, self.top_left, self.top_right, self.bottom_left, self.bottom_right)
+    #     else:
+    #         super().__delattr__(name)
+
+class Rect(pygame.Rect):
+    def __init__(
+            self,
+            surface: pygame.Surface,
+            color: pygame.Color,
+            size: pygame.Vector2,
+            pos: pygame.Vector2,
+            width: int = 0,
+            border: Border = Border()
+            ):
+        
+        super().__init__(pos, size)
+
+        self.surface: pygame.Surface = surface
+        self.color: pygame.Color = color
+
+        self.rect: pygame.Rect = pygame.Rect(pos, size)
+        self.size: pygame.Vector2 = size
+        self.pos: pygame.Vector2 = pos
+
+        self.width: int = width
+        self.border = border
+    
+    def draw(
+        self,
+        surface: pygame.Surface | None = None,
+        color: pygame.Color | None = None,
+        rect: pygame.Rect | None = None,
+        width: int = None,
+        border: Border | None = None,
+    ):
+        """Draws self to self.surface"""
+
+        #o short for output
+        if surface == None:
+            o_surface = self.surface
+        else:
+            o_surface = surface
+        
+        if color == None:
+            o_color = self.color
+        else:
+            o_color = color
+        
+        if rect == None:
+            o_rect = self.rect
+        else:
+            o_rect = rect
+
+        if width == None:
+            o_width = self.width
+        else:
+            o_width = width
+        
+        short_in_func = self.border.is_shortened
+        default_in_func = self.border.is_default
+        
+        def check_validity(obj, other_return_object):
+            if obj == -1:
+                return other_return_object
+            else:
+                return obj
+
+        if border == None:
+            if short_in_func:
+                o_border_short = self.border.shortened
+            o_border_full = self.border
+        else:
+            default_in_func = False
+            if border.is_shortened:
+                o_border_short = border.shortened
+            
+            o_border_full = Border(
+                -1,
+                check_validity(border[0], self.border[0]),
+                check_validity(border[1], self.border[1]),
+                check_validity(border[2], self.border[2]),
+                check_validity(border[3], self.border[3]),
+            )
+            if not o_border_full.is_default:
+                short_in_func = False
+                default_in_func = False
+        
+        print(border)
+        print(o_border_full)
+
+        #o is short for output
+        match (short_in_func, default_in_func, o_width):
+            case (True, True, 0):
+                print("176")
+                pygame.draw.rect(o_surface, o_color, o_rect)
+            case (True, False, 0):
+                print("179")
+                pygame.draw.rect(o_surface, o_color, o_rect, border_radius=o_border_short)
+            case (True, False, _):
+                print("182")
+                pygame.draw.rect(o_surface, o_color, o_rect, o_width, o_border_short)
+            case (True, True, _):
+                print("185")
+                pygame.draw.rect(o_surface, o_color, o_rect, o_width)
+            case (False, False, _):
+                print("191")
+                print(o_border_full[3])
+                pygame.draw.rect(o_surface,
+                    o_color,
+                    o_rect,
+                    o_width,
+                    border_top_left_radius=o_border_full[0],
+                    border_top_right_radius=o_border_full[1],
+                    border_bottom_left_radius=o_border_full[2],
+                    border_bottom_right_radius=o_border_full[3]
+                )
+
+#         print(f"139| <{self.color=}> <{self.rect=}> <{self.width=}> <{self.border=}> <{self.border.shortened=}>\n \
+# <{self.border.is_shortened=}> <{self.border.default=}> <{self.surface}>\n")
+        return self
+
 mouse = Mouse()
 
-playButton = pygame.sprite.Sprite()
+playButton = Rect(screen, (255, 255, 255), (200, 100), (300, 500))
 
 while True:
+
+    screen.fill((3, 6, 22))
+    # screen.fill((125, 125, 125))
 
     all_events = pygame.event.get()
     for event in all_events:
@@ -61,7 +267,6 @@ while True:
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse.pos = event.pos
             if event.button == 1:
                 mouse.state.mb1 = True
                 mouse.state[0] = True
@@ -78,7 +283,6 @@ while True:
                 mouse.state.scroll_down = True
                 mouse.state[4] = True
         elif event.type == pygame.MOUSEBUTTONUP:
-            mouse.pos = event.pos
             if event.button == 1:
                 mouse.state.mb1 = False
                 mouse.state[0] = False
@@ -94,9 +298,9 @@ while True:
             elif event.button == 5:
                 mouse.state.scroll_down = False
                 mouse.state[4] = False
-        print(f"{mouse.state=}")
+    mouse.pos = pygame.mouse.get_pos()
     
-    screen.fill((3, 6, 22))
+    playButton.draw()
 
     pygame.display.update()
     clock.tick(60)
--- testing/base_test copy.py
+++ testing/base_test copy.py
@@ -7,21 +7,6 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 
-class Coordinate:
-    def __init__(self, x: float, y: float):
-
-        x = float(x)
-        y = float(y)
-
-        super(tuple).__init__(tuple, (x, y))
-
-        self.x = x
-        self.y = y
-        print("correct")
-    
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
 while True:
     events = pygame.event.get()
     for event in events:
@@ -31,6 +16,8 @@ while True:
     
     screen.fill((0,0,0))
 
+    pygame.draw.rect(screen, (255, 255, 255), (300, 500, 200, 100))
+    
+
     pygame.display.update()
     clock.tick(60)
-
