05f57c3a07ef1fb58485edb1d83284368a8ad290
AlexM-2 <198247789+AlexM-2@users.noreply.github.com>
2025-06-28 12:41:16 +0100

    Add foundations for svg support *
    Using xml.etree.ElementTree to extract data from an svg file
    Create SVG class to represent an svg file and a Rect class to
    represent a rect element in an svg file with a draw function that
    draws itself onto a specified pygame.Surface object. Reorganised
    code with a menu_screen function that draws the menu screen.
    The Sprite class now has an image parameter in instantiation that
    is used as it's "image" attribute and supplies the Sprite's "size"
    attribute "size" instantiation parameter has been replaced with
    "pos".

--- testing/base_test copy.py
+++ testing/base_test copy.py
@@ -8,14 +8,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 screen.fill((255, 255, 255))
-surf = pygame.Surface((200, 200), pygame.SRCALPHA)
-pygame.draw.rect(
-    surf,
-    (0, 0, 255, 125),
-    (0, 0, 200, 200)
-)
 
-screen.blit(surf, (0, 0))
+def draw(output_surface: pygame.Surface):
+    pygame.draw.rect(output_surface, (255, 0, 0), (200, 200, 200, 200))
+
+draw(screen)
 
 pygame.display.update()
 
@@ -25,5 +22,5 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-
+    
     clock.tick(20)
